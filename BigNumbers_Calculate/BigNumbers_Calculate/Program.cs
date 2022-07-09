7265895dbb378a9a841d1a819a116485f477e2aa
Zion1314 <minovirgo468@gmail.com>
2022-07-09 12:29:07 +0700

    Fix source: Array -> String calculate

--- BigNumbers_Calculate/BigNumbers_Calculate/Program.cs
+++ BigNumbers_Calculate/BigNumbers_Calculate/Program.cs
@@ -9,105 +9,203 @@ namespace BigNumbers_Calculate
         {
             string exit = "";
             // Input
-            while (exit != "x")
-            {
-                Console.Clear();
-                Console.WriteLine("Enter a & b:");
-                Console.Write("  a = ");
-                string a = Console.ReadLine();
-                Console.Write("  b = ");
-                string b = Console.ReadLine();
+            Console.WriteLine("Enter a & b:");
+            Console.Write("  a = ");
+            string a = Console.ReadLine();
+            Console.Write("  b = ");
+            string b = Console.ReadLine();
 
-
-                MyBigNumber cal = new MyBigNumber(a, b);
-                String result = cal.sum();
-                Console.WriteLine("Result for a + b is " + result);
-
-                Console.Write("Enter 'x' and Enter to exit program:");
-                exit = Console.ReadLine();
-            }
-            
+            MyBigNumber cal = new MyBigNumber();
+            String result = cal.sum(a, b);
+            Console.WriteLine("Result for a + b is " + result);
         }
     }
 
     class MyBigNumber
     {
-        private char[] lista,listb;
+        public MyBigNumber()
+        {}
 
-        public MyBigNumber(string n1,string n2)
+        public String sum(string n1, string n2)
         {
-            // String Lenght Balance
-            if (n1.Length > n2.Length)
+            // Declare default variable
+            int nLenght1 = n1.Length;
+            int nLenght2 = n2.Length;
+
+
+            Console.Clear();
+            Console.WriteLine("a = "+n1);
+            Console.WriteLine("b = "+n2);
+            Console.WriteLine();
+
+            int step = 0, count = 1, rem = 0;
+            String result = "";
+            
+            Console.WriteLine("Start: ");
+            if (nLenght1 >= nLenght2)
             {
-                int gap = n1.Length - n2.Length;
-                for(int i = 0; i < gap; i++)
+                int gap = nLenght1 - nLenght2;
+                while (count <= nLenght1 - gap)
                 {
-                    n2 = "0" + n2;
+                    step++;
+                    int tmpsum;
+                    if (nLenght2 - count > 0)
+                    {
+                        tmpsum = (n1[nLenght1 - count] - '0') + (n2[nLenght2 - count] - '0') + rem;
+                    }
+                    else
+                    {
+                        if (nLenght1 - count > 0)
+                        {
+                            gap = gap - 1;
+                        }
+
+
+                        if (nLenght2 - count == 0)
+                        {
+                            tmpsum = (n1[nLenght1 - count] - '0') + (n2[nLenght2 - count] - '0') + rem;
+                        }
+                        else { 
+                            tmpsum = (n1[nLenght1 - count] - '0') + rem;
+                        }
+                    }
+
+                    if (tmpsum >= 10)
+                    {
+                        result = (tmpsum - 10) + result;
+
+                        if (nLenght2 - count >= 0)
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + (tmpsum - 10) + ", rem = 1");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + (tmpsum - 10) + ", rem = 1");
+                        }
+
+                        rem = 1;
+                    }
+                    else
+                    {
+                        result = tmpsum + result;
+
+                        if (nLenght2 - count >= 0)
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + tmpsum + ", rem = 0");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + tmpsum + ", rem = 0");
+                        }
+
+                        rem = 0;
+                    }
+
+
+                    if (nLenght1-count==0 & rem == 1)
+                    {
+                        result = '1' + result;
+                        Console.WriteLine(" Step " + step + ": Add 1(rem). End");
+                    }
+
+                    if(rem==0 & nLenght2 - count < 0)
+                    {
+                        String tmpresult="";
+                        for(int i = nLenght1 - count - 1; i >= 0; i--)
+                        {
+                            tmpresult = n1[i] + tmpresult;
+                        }
+                        Console.WriteLine(" Step " + (++step) + ": no rem -> Put down all uncalculated digits.End");
+                        result = tmpresult + result;
+                        count = nLenght1;
+                    }
+                    count++;
                 }
             }
             else
             {
-                int gap = n2.Length - n1.Length;
-                for (int i = 0; i < gap; i++)
+                int gap = nLenght2 - nLenght1;
+                while (count <= nLenght2 - gap)
                 {
-                    n1 = "0" + n1;
-                }
-            }
-
-            //String to CharArray
-            this.lista = n1.ToCharArray();
-            this.listb = n2.ToCharArray();
-
-            //Console.WriteLine(this.lista);
-            //Console.WriteLine(this.listb);
-        }
-
-        public String sum()
-        {
-            // Declare default variable
-            int nLenght = this.lista.Length;
-            int rem = 0; // 0 or 1
-            int step = 0;
-            String result="";
-            // Calculate at each position last -> first
-            Console.WriteLine("Start:");
-            while (nLenght != 0)
-            {
-                step += 1;
-                int tmpsum = 0; 
-                // Char to int: number = int(char - '0')
-                int n1 = Convert.ToInt32(this.lista[nLenght - 1] - '0');
-                int n2 = Convert.ToInt32(this.listb[nLenght - 1] - '0');
-
-                if (rem == 0){
-                    tmpsum = n1 + n2;
-                }
-                else
-                {
-                    tmpsum = n1 + n2 + rem;
-                }
-
-                if (tmpsum >= 10)
-                {
-                    result = (tmpsum - 10) + result;
-                    if (nLenght - 1 == 0)
+                    step++;
+                    int tmpsum;
+                    if (nLenght1 - count > 0)
                     {
-                        result = 1 + result;
-                        Console.WriteLine(" Step " + step + ": Add 1(rem). End");
+                        tmpsum = (n2[nLenght2 - count] - '0') + (n1[nLenght1 - count] - '0') + rem;
                     }
-                    else {
-                        Console.WriteLine(" Step " + step + ": "+ n1+" + " + n2 + " + " + rem + "(rem) = " + tmpsum + ". Add " + (tmpsum-10) + ", remember 1");
+                    else
+                    {
+                        if (nLenght2 - count > 0)
+                        {
+                            gap = gap - 1;
+                        }
+
+
+                        if (nLenght1 - count == 0)
+                        {
+                            tmpsum = (n2[nLenght2 - count] - '0') + (n1[nLenght1 - count] - '0') + rem;
+                        }
+                        else
+                        {
+                            tmpsum = (n2[nLenght2 - count] - '0') + rem;
+                        }
+                    }
+
+                    if (tmpsum >= 10)
+                    {
+                        result = (tmpsum - 10) + result;
+
+                        if (nLenght1 - count >= 0)
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + (tmpsum - 10) + ", rem = 1");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + (tmpsum - 10) + ", rem = 1");
+                        }
+
                         rem = 1;
                     }
+                    else
+                    {
+                        result = tmpsum + result;
+
+                        if (nLenght1 - count >= 0)
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n1[nLenght1 - count] + " + " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + tmpsum + ", rem = 0");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Step " + step + ": " + n2[nLenght2 - count] + " + " + rem + "(rem) = " + tmpsum + ". Add " + tmpsum + ", rem = 0");
+                        }
+
+                        rem = 0;
+                    }
+
+
+                    if (nLenght2 - count == 0 & rem == 1)
+                    {
+                        result = '1' + result;
+                        Console.WriteLine(" Step " + step + ": Add 1(rem). End");
+                    }
+
+                    if (rem == 0 & nLenght1 - count < 0)
+                    {
+                        String tmpresult = "";
+                        for (int i = nLenght2 - count - 1; i >= 0; i--)
+                        {
+                            tmpresult = n2[i] + tmpresult;
+                        }
+                        Console.WriteLine(" Step " + (++step) + ": no rem -> Put down all uncalculated digits.End");
+                        result = tmpresult + result;
+                        count = nLenght2;
+                    }
+                    count++;
                 }
-                else
-                {
-                    result = tmpsum + result;
-                    Console.WriteLine(" Step " + step + ": " + n1 + " + " + n2 + " + " + rem + "(rem) = " + tmpsum + ". Add " + tmpsum + ", remember 0");
-                    rem = 0;
-                }
-                nLenght -= 1;
             }
+
+            Console.WriteLine();
+ 
             return result;
         }
     }
